dde7adc6430b779ba07ea307877dfdf7928b96bd
Bruno Fernandes <bfdes@users.noreply.github.com>
2021-02-28 19:06:05 +0000

    Solution to deploy to GitHub pages

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf site
+
+yarn compile:prod
+yarn build:prod
+
+cd site
+git init
+git remote add origin git@github.com:bfdes/bfdes.github.io.git
+git add .
+git commit --allow-empty-message -m ""
+git push --force origin master:gh-pages
