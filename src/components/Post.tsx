2995d603a7d40089142e90500d40ec2867a6e1be
Bruno Fernandes <bfdes@users.noreply.github.com>
2021-03-05 21:15:16 +0000

    TS esModuleInterop

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Date from "./Date";
 import PaginationLink from "./PaginationLink";
 import TagList from "./TagList";
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import TagList from "./TagList";
 import Date from "./Date";
 import WordCount from "./WordCount";
